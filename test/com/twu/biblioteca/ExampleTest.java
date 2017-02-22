ef93278ab3949c90a84d6ac6f6a617a378e5b9b5
imac <1294749550@qq.com>
2017-02-22 21:31:00 +0800

    add test cases

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,8 +1,9 @@
 package com.twu.biblioteca;
 
 import java.io.*;
+import java.util.*;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ExampleTest {
 
@@ -13,5 +14,44 @@ public class ExampleTest {
         System.setOut(new PrintStream(outContent));
         BibliotecaApp.welcomeMessage();
         assertEquals("Hello, welcome to Biblioteca!\n", outContent.toString());
+    }
+
+    @Test
+    public void testListBooks() {
+        new Library();
+        List<String> availableBooksInfo = Library.listBooks();
+        int originSize = availableBooksInfo.size();
+        assertEquals(7, originSize);
+        for (String bookInfo : availableBooksInfo) {
+            assertTrue(bookInfo.contains("Author:"));
+            assertTrue(bookInfo.contains("Year published:"));
+        }
+    }
+
+    @Test
+    public void testCheckoutABook() {
+        new Library();
+        assertTrue(!Library.checkoutABook(-2));
+        assertTrue(!Library.checkoutABook(16));
+        assertTrue(Library.checkoutABook(3));
+        List<String> newAvailableBooksInfo = Library.listBooks();
+        int newSize = newAvailableBooksInfo.size();
+        assertEquals(6, newSize);
+        boolean noCheckoutBook = true;
+        for (String bookInfo : newAvailableBooksInfo) {
+            if (bookInfo.contains("ID: 3")) {
+                noCheckoutBook = false;
+            }
+        }
+        assertTrue(noCheckoutBook);
+    }
+
+    @Test
+    public void testReturnABook() {
+        new Library();
+        assertTrue(!Library.returnABook(-2));
+        assertTrue(!Library.returnABook(16));
+        Library.checkoutABook(3);
+        assertTrue(Library.returnABook(3));
     }
 }
