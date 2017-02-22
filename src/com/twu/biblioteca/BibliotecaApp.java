aaaca7405614ed3ef6cf40261f3d2539f2b7f154
imac <1294749550@qq.com>
2017-02-22 11:56:52 +0800

    Add welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        welcomeMessage();
+    }
+
+    protected static void welcomeMessage() {
+        String welcome = "Hello, welcome to Biblioteca!";
+        System.out.println(welcome);
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,17 @@
 package com.twu.biblioteca;
 
-
+import java.io.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private BibliotecaApp app = new BibliotecaApp();
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        app.welcomeMessage();
+        assertEquals("Hello, welcome to Biblioteca!\n", outContent.toString());
     }
 }
